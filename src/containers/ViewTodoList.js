760a6e8ec00dee5ba45e9128428eeb1fa0350740
Iggy <igoririanto@rocketmail.com>
2017-12-05 06:58:29 -0800

    displays todoList

--- src/containers/ViewTodoList.js
+++ src/containers/ViewTodoList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {connect} from 'react-redux';
 import TodoList from '../components/TodoList';
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state) => ({
   todos: state.todos
-}
+})
 
 const ViewTodoList = connect(
   mapStateToProps
